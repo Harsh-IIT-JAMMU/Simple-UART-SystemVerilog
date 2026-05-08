0f930dc1009011f10bbdea186e55f850c7e07c16
Harsh-IIT-JAMMU <2025psp0003@iitjammu.ac.in>
2026-05-08 18:10:01 +0530

    Create tb_pkg.sv

---
+++ TestBench/tb_pkg.sv
@@ -0,0 +1,11 @@
+package tb_pkg;
+
+  `include "interface.sv"
+  `include "transaction.sv"
+  `include "generator.sv"
+  `include "driver.sv"
+  `include "monitor.sv"
+  `include "scoreboard.sv"
+  `include "environment.sv"
+
+endpackage
