5e8299f986ce4a02de33336ea9b56434094950d3
Harsh-IIT-JAMMU <2025psp0003@iitjammu.ac.in>
2026-05-08 17:51:58 +0530

    Update TestBench_UART_Top.sv

--- TestBench/TestBench_UART_Top.sv
+++ TestBench/TestBench_UART_Top.sv
@@ -42,3 +42,17 @@ module tb;
   assign vif.uclkrx = dut.rtx.uclk;  // RX clock
 
 endmodule
+/*
+1) TestBench_UART_Top.sv
+
+Testbench = connects DUT + starts environment + generates clock + enables observation
+Execution flow:
+1. Clock starts
+2. env.run()
+3. Generator creates transaction
+4. Driver drives DUT
+5. DUT processes
+6. Monitor observes
+7. Scoreboard checks
+8. Repeat 5 times
+*/
