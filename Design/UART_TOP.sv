bfe29a151d2576ca25c11418dbd1e441cf098746
Harsh-IIT-JAMMU <2025psp0003@iitjammu.ac.in>
2026-05-13 08:17:27 +0530

    Update UART_TOP.sv

--- Design/UART_TOP.sv
+++ Design/UART_TOP.sv
@@ -1,31 +1,61 @@
 `timescale 1ns / 1ps
- 
+
 module uart_top
 #(
-parameter clk_freq = 1000000,
-parameter baud_rate = 9600
+    parameter clk_freq  = 1000000,
+    parameter baud_rate = 9600
 )
 (
-  input clk,rst, 
-  input rx,
-  input [7:0] din_tx,
-  input newd,
-  output tx, 
- output [7:0] rxdata,
-  output done_tx,
-  output done
-    );
-    
-uarttx 
-#(clk_freq, baud_rate) 
-utx   
- (clk, rst, newd, din_tx, tx, done_tx);   
- 
-uartrx 
-#(clk_freq, baud_rate)
+    input clk,
+    input rst,
+
+    input rx,
+
+    input [7:0] din_tx,
+    input newd,
+
+    output tx,
+    output [7:0] rxdata,
+
+    output done_tx,
+    output done
+);
+
+/////////////////////////////////////////////////
+// UART TRANSMITTER
+/////////////////////////////////////////////////
+
+uart_tx
+#(
+    .clk_freq(clk_freq),
+    .baud_rate(baud_rate)
+)
+utx
+(
+    .clk(clk),
+    .rst(rst),
+    .newd(newd),
+    .din_tx(din_tx),
+    .tx(tx),
+    .done_tx(done_tx)
+);
+
+/////////////////////////////////////////////////
+// UART RECEIVER
+/////////////////////////////////////////////////
+
+uartrx
+#(
+    .clk_freq(clk_freq),
+    .baud_rate(baud_rate)
+)
 rtx
- (clk, rst, rx, done, rxdata);    
-    
-    
+(
+    .clk(clk),
+    .rst(rst),
+    .rx(rx),
+    .done(done),
+    .rxdata(rxdata)
+);
+
 endmodule
- 
